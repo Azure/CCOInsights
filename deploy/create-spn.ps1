606adbdc69a0ee96968c660b9211510563cc2217
drdonoso <davidrodriguezdonoso95@gmail.com>
2025-03-28 11:08:38 +0100

    Change script deployment spn

--- deploy/create-spn.ps1
+++ deploy/create-spn.ps1
@@ -8,4 +8,5 @@ Write-Host "AZURE_CREDENTIALS:"
 Write-Host ($jsonResponse | ConvertFrom-Json | ConvertTo-Json) -ForegroundColor Yellow
 Write-Host "Please copy the above output and save it in a secure location."
 Write-Host "Create a new secret in the GitHub repository and paste the above output as the value."
+Write-Host "⚠️ Please remebmer to add the following graph permissions to the service principal: Application.Read.All, Group.Read.All, User.Read.All"
 Write-Host "Script completed."

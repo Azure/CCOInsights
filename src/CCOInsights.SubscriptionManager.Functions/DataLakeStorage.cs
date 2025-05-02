c5848b821aff365c316648b10246e866ec776cfa
drdonoso <davidrodriguezdonoso95@gmail.com>
2025-05-02 12:26:52 +0200

    Add UpperCamelCaseNamingStrategy

--- src/CCOInsights.SubscriptionManager.Functions/DataLakeStorage.cs
+++ src/CCOInsights.SubscriptionManager.Functions/DataLakeStorage.cs
@@ -1,6 +1,9 @@
-﻿using Azure.Storage.Files.DataLake;
+﻿using System.Globalization;
+using Azure.Storage.Files.DataLake;
 using Azure.Storage.Files.DataLake.Models;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace CCOInsights.SubscriptionManager.Functions;
 
@@ -22,7 +25,13 @@ public class DataLakeStorage(DataLakeServiceClient dataLakeServiceClient, ILogge
             var fileSystem = dataLakeServiceClient.GetFileSystemClient(containerName);
 
             var response = fileSystem.GetFileClient(id);
-            var stream = BinaryData.FromObjectAsJson(item).ToStream();
+            var json = JsonConvert.SerializeObject(item, new JsonSerializerSettings
+            {
+                ContractResolver = new UpperCamelCaseNamingResolver(),
+                Formatting = Formatting.Indented,
+            }
+        );
+        var stream = new MemoryStream(Encoding.UTF8.GetBytes(json));
 
             await response.UploadAsync(stream, new DataLakeFileUploadOptions{HttpHeaders = new PathHttpHeaders{ContentType = "application/json"}}, cancellationToken);
         }
@@ -31,5 +40,13 @@ public class DataLakeStorage(DataLakeServiceClient dataLakeServiceClient, ILogge
             _logger.LogError($"Error processing {typeof(T).Name} with id: {id}. Exception: {ex}");
             throw;
         }
+    }
+}
+
+public class UpperCamelCaseNamingResolver: DefaultContractResolver
+{
+    protected override string ResolvePropertyName(string propertyName)
+    {
+        return char.ToUpper(propertyName[0], CultureInfo.InvariantCulture) + propertyName.Substring(1);
     }
 }

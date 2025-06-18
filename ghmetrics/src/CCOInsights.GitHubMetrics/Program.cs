6e318c2a553baa7e24a1a770d6ac8ad2954cac18
DrDonoso <davidrodriguezdonoso95@gmail.com>
2025-06-18 12:45:32 +0200

    Add csproj with quartz and move web

---
+++ ghmetrics/src/CCOInsights.GitHubMetrics/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using Quartz;
+
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddQuartz(q =>
+        {
+            q.ScheduleJob<GitHubMetricsExporterJob>(x => x
+                .WithIdentity(nameof(GitHubMetricsExporterJob))
+                .WithCronSchedule(GitHubMetricsExporterJob.CronSchedule, s => s.InTimeZone(TimeZoneInfo.Utc)));
+
+        });
+
+        services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+    });
+
+await builder.RunConsoleAsync();
+
+public class GitHubMetricsExporterJob : IJob
+{
+    //public static string CronSchedule = "0 0 10 * * ?"; // Execute daily at 10 AM UTC
+    public static string CronSchedule = "0 * * * * ?"; // Execute every minute
+
+    public Task Execute(IJobExecutionContext context)
+    {
+        Console.WriteLine("Hello, World!");
+        return Task.CompletedTask;
+    }
+}
